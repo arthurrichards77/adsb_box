028fbc2db5d39b23ece07c49117e9c49f1bd7c17
aeagr-l <arthur.richards@bristol.ac.uk>
2023-07-13 09:28:46 +0100

    Longer per file upload and longer run overall

--- record.py
+++ record.py
@@ -8,14 +8,14 @@ parser.add_argument('--token', required=True,
                     '(see https://www.dropbox.com/developers/apps)')
 args = parser.parse_args()
 
-for ii in range(100):
+for ii in range(10000):
   fields_to_save = [4,6,7,11,14,15]
   call_signs = {}
   dt_now = datetime.now()
   filename = f'data_{dt_now.year}_{dt_now.month}_{dt_now.day}_{dt_now.hour}_{dt_now.minute}_{dt_now.second}.csv'
   f = open(filename, "w")
   print('Opened file {}'.format(filename))
-  for jj in range(10000):
+  for jj in range(20000):
     line_in = input()
     fields = line_in.split(',')
     if fields[1]=='1':
