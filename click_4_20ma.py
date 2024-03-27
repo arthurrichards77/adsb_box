be892555e27e2f4d8915bcf7e979deecc9f0e978
Arthur on the Pi <arthur.richards@bristol.ac.uk>
2024-03-27 12:21:56 +0000

    CLick 4-20ma is now a library for further use

--- click_4_20ma.py
+++ click_4_20ma.py
@@ -1,27 +1,36 @@
 import spidev
 import time
 
-spi = spidev.SpiDev()
+def get_click_current(device_id=1):
+    spi = spidev.SpiDev()
+    spi.open(0,device_id)
+    spi.max_speed_hz = 20000
+    spi.mode = 0
+    data = spi.readbytes(2)
+    adc = ((data[0] & 0x1F) << 7) | (data[1] >> 1)
+    curr_ma = adc/200
+    return curr_ma
 
-spi.open(0,1)
+def main():
+    spi = spidev.SpiDev()
+    spi.open(0,1)
+    spi.max_speed_hz = 20000
+    spi.mode = 0
+    while True:
+        data = spi.readbytes(3)
+        print(data)
+        print(f'{data[0]:08b} {data[1]:08b} {data[2]:08b}')
+        adc = int(data[0]*128 + data[1]/2)
+        print(adc)
+        adc2 = ((data[0] & 0x1F) << 7) | (data[1] >> 1)
+        print(adc2)
+        volts = 2.048*adc/4096
+        print(volts)
+        curr_ma = 1000*(volts/20)/4.99
+        print(curr_ma)
+        curr_ma2 = adc2/200
+        print(curr_ma2)
+        time.sleep(1)
 
-spi.max_speed_hz = 20000
-spi.mode = 0
-
-while True:
-    data = spi.readbytes(3)
-    print(data)
-    print(f'{data[0]:08b} {data[1]:08b} {data[2]:08b}')
-    adc = int(data[0]*128 + data[1]/2)
-    print(adc)
-    adc2 = ((data[0] & 0x1F) << 7) | (data[1] >> 1)
-    print(adc2)
-    volts = 2.048*adc/4096
-    print(volts)
-    curr_ma = 1000*(volts/20)/4.99
-    print(curr_ma)
-    curr_ma2 = adc2/200
-    print(curr_ma2)
-    time.sleep(1)
-
-
+if __name__=='__main__':
+    main()
