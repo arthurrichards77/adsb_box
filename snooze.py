e966389099ae1bad556c93091918346c3ace9c64
Arthur Richards <arthurrichards77@users.noreply.github.com>
2024-04-11 11:06:00 +0100

    Create snooze.py

---
+++ snooze.py
@@ -0,0 +1,26 @@
+import sys
+from datetime import datetime,timedelta
+from smbus import SMBus
+
+i2c_bus = 1
+i2c_dev = 4
+
+#print(sys.argv)
+assert len(sys.argv)==3
+
+off_delay = int(sys.argv[1])
+assert off_delay>0
+assert off_delay<256
+
+snooze_time = int(sys.argv[2])
+assert snooze_time>0
+assert snooze_time<65536
+
+off_time = datetime.now()+timedelta(seconds=5*off_delay)
+on_time = off_time + timedelta(seconds=5*snooze_time)
+print(f'Off at {off_time}')
+print(f'Back on at {on_time}')
+
+bus = SMBus(i2c_bus)
+bus.write_block_data(i2c_dev,off_delay,[snooze_time])
+bus.close()
